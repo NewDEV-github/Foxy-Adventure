32c3616309d6671e2325a1359fd88d20f0b834b4
MasterPolska123 <karoltomaszewskimusic@gmail.com>
2020-08-22 16:15:08 +0200

    new functions to our dlc system

--- Scripts/ServerAPI/dlc_loader.gd
+++ Scripts/ServerAPI/dlc_loader.gd
@@ -46,23 +46,30 @@ func load_all_dlcs():
 	else:
 		print("An error occurred when trying to access the path.")
 
+
 func download_pck_files_from_cfg(cfg_name:String):
+	dir.open("user://dlcs/")
+	var file_name = ""
 	var info_file = load("user://dlcs/" + cfg_name + ".gd").new()
+	dir.make_dir(str(info_file.dlc_name))
 	var files_array = info_file.dlc_files
 	print(str(files_array))
-	for file in files_array:
-		print(str(file))
-		$download_dlc_files.set_download_file("user://dlcs/" + str(cfg_name) + "_" + str(pck_number) + ".pck")
-		$download_dlc_files.request(file)
-		yield($download_dlc_files, "request_completed")
-		pck_number += 1
-	
+	for f_name in info_file.dlc_file_names:
+		file_name = str(f_name)
+		for file in files_array:
+			print(str(file))
+			$download_dlc_files.set_download_file("user://dlcs/" + str(info_file.dlc_name) + "/" + str(file_name) + ".pck")
+			$download_dlc_files.request(file)
+			yield($download_dlc_files, "request_completed")
+			pck_number += 1
+
 func download_dlc_list():
 	print('downloading dlc list')
 	$download_dlc_list.set_download_file("user://dlcs/dlc_list.gd")
 	$download_dlc_list.request("https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_list.gd")
 	
 func download_dlc_cfg_files():
+	dir.open("user://dlcs/")
 	var script = load("user://dlcs/dlc_list.gd").new()
 	Globals.dlc_name_list = script.dlc_list
 	print(str(Globals.dlc_name_list))
@@ -72,12 +79,18 @@ func download_dlc_cfg_files():
 		$download_dlc_cfg.set_download_file("user://dlcs/" + str(cfg_number) + '.gd')
 		$download_dlc_cfg.request(cfg)
 		yield($download_dlc_cfg, "request_completed")
+		var new_filename = load("user://dlcs/" + str(cfg_number) + '.gd').new().dlc_name
+		dir.rename(str(cfg_number) + ".gd", str(new_filename) + ".gd")
 		cfg_number += 1
 	list_downloaded = true
-	download_pck_files_from_cfg("1")
-	download_pck_files_from_cfg("2")
-	download_pck_files_from_cfg("3")
+	download_pck_files_from_cfg("Classic Sonic")
+	download_pck_files_from_cfg("Test")
 
 
 func _on_download_dlc_list_request_completed(result, response_code, headers, body):
 	download_dlc_cfg_files()
+
+func uninstall_dlc(dlc_name:String):
+	var dlc_cfg_file = load("user://dlcs/" + dlc_name + ".gd").new()
+	dir.open("user://dlcs/")
+	dir.remove(str(dlc_cfg_file.dlc_name))
