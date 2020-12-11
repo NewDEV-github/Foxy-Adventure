ce1af79c6b1d7936d288d539dc229094b044f1e6
JezSonic <karoltomaszewskimusic@gmail.com>
2020-12-11 16:14:25 +0100

    ntf is back

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -42,8 +42,7 @@ var version_string:String = "alpha"
 var version_commit:String = "unknown"
 var current_save_name = ""
 var new_characters:Array = [
-#	"NewTheFox",
-#	"Tails.exe",
+	"NewTheFox",
 	"Tails",
 ]
 func construct_game_version():
--- dlcs/dlc_tails_exe.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-var pack_name = 'dlc_tails_exe'
-func add_characters():
-	Globals.add_character("Tails.exe")
-	Globals.add_character('Brad "Snake" Prower')
-func add_stages():
-	pass
-func add_dlc():
-	Globals.add_dlc("Tails.exe")
