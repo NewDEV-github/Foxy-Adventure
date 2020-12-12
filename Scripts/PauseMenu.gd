91147ae25f887fc7414ba078301c25811a821418
JezSonic <karoltomaszewskimusic@gmail.com>
2020-12-12 17:18:59 +0100

    fix all bugs in pause menu

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -2,26 +2,13 @@ extends WindowDialog
 var visible_connect
 var audio = AudioServer
 var music_bus_idx = audio.get_bus_index('Music')
-var pausemenu_bus_idx = audio.get_bus_index('PauseMenu')
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		visible = !visible
-		if visible:
-			audio.set_bus_mute(music_bus_idx, true)
-			audio.set_bus_mute(pausemenu_bus_idx, false)
-			$MusicDelay.start()
-		else:
-			audio.set_bus_mute(music_bus_idx, false)
-			audio.set_bus_mute(pausemenu_bus_idx, true)
-			$MusicDelay.stop()
-
-
-func _on_visibility_changed():
 	get_tree().paused = visible
 
 func _on_Resume_pressed():
-	hide()
-	$PauseMenu.stop()
+	visible = !visible
 
 
 func _on_Options_pressed():
@@ -32,13 +19,7 @@ func _on_QuitGame_pressed():
 	$QuitTOMenuDIalog.popup_centered()
 
 func _on_QuitTOMenuDIalog_confirmed():
-	get_tree().quit()
-	get_tree().paused = false
-#	BackgroundLoad.play_start_transition = false
-	audio.set_bus_mute(music_bus_idx, false)
-	audio.set_bus_mute(pausemenu_bus_idx, true)
-	$MusicDelay.stop()
-	$PauseMenu.stop()
+	visible = !visible
 	get_tree().change_scene('res://Scenes/Menu.tscn')
 
 
