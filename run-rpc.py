9ddf2a2bb3b9c487a4660f20fc0efcd130e2ae4a
Neo <67120154+NewTheFox@users.noreply.github.com>
2021-01-08 16:18:29 +0100

    Doo do do doo doo doo dooo Doo do do doo doo doo dooo Doo do do doo doo doo dooo! Doo do do doo doo doo dooo
    (Uganda-ganda-ganda...)
    
    Do you know da wae, now know da wae?
    I do not know da wae
    We must find, now we not find da wae
    Yeah, we must find da wae
    
    Can you show me da wae?
    Can you show me, can you show me da wae?
    We must find da wae
    We must find, we must find da wae
    
    Do you know da wae? I don't know da wae
    We must find da wae
    Do you know da wae? I don't know da wae
    We must find da wae
    
    Can you show me da wae?
    Can you show me, can you show me da wae?
    We must find da wae
    We must find, we must find da wae
    
    Can you show me da wae?
    Can you show me, can you show me da wae?
    We must find da wae
    We must find, we must find da wae
    
    (Uganda-ganda-ganda...)

--- run-rpc.py
+++ run-rpc.py
@@ -2,7 +2,7 @@ import rpc
 import time
 from time import mktime
 
-client_id = '467570845993271307'  # Your application's client ID as a string. (This isn't a real client ID)
+client_id = '729429191489093702'  # Your application's client ID as a string. (This isn't a real client ID)
 rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)  # Send the client ID to the rpc module
 print("RPC connection successful.")
 
