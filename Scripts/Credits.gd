441b4a58f2e944ad72930b974a113d2330c4259c
JezSonic <karoltomaszewskimusic@gmail.com>
2021-01-14 19:40:51 +0100

    credits update - part 2

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -5,7 +5,7 @@ func _ready():
 	f.open("gekagd.txt", File.READ)
 	$HBoxContainer/RichTextLabel.bbcode_text += "[center]\nPowered by Godot Engine\nFoxy Adventure - Copyright 2020 - "\
 	+ str(OS.get_date().year) + " - New DEV\nGodot Engine - Copyright 2020 - " +\
-	str(OS.get_date().year) + " - Godot Engine Contributors[/center]"#SilnyGekon
+	str(OS.get_date().year) + " - Godot Engine Contributors\n\nGekon, our team wants to say big 'Thank You' to You\nfor putting a lot of non-obligatory job into the game.[/center]"#SilnyGekon
 	$AnimationPlayer.play("credits")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
