6361aa8d655da561eba87fa1927531adc9303a52
JezSonic <karoltomaszewskimusic@gmail.com>
2021-04-25 14:26:32 +0200

    added new richtexteffects

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -7,7 +7,7 @@ func _ready():
 	f.open("gekagd.txt", File.READ)
 	$HBoxContainer/RichTextLabel.bbcode_text += "[center]\nPowered by Godot Engine\nFoxy Adventure - Copyright 2020 - "\
 	+ str(OS.get_date().year) + " - New DEV\nGodot Engine - Copyright 2020 - " +\
-	str(OS.get_date().year) + " - Godot Engine Contributors\n\nGekon, our team wants to say big 'Thank You' to You\nfor putting a lot of non-obligatory job into the game.[/center]"#SilnyGekon
+	str(OS.get_date().year) + " - Godot Engine Contributors"#SilnyGekon
 	$AnimationPlayer.play("credits")
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
---
+++ assets/RichTextEffects/pulse.gd
@@ -0,0 +1,20 @@
+tool
+extends RichTextEffect
+class_name RichTextPulse
+
+var bbcode = "pulse"
+
+func _init():
+	resource_name = "RichTextPulse"
+
+func _process_custom_fx(char_fx : CharFXTransform) -> bool:
+	var color = char_fx.env.get("color", char_fx.color)
+	var height = char_fx.env.get("height", 0.0)
+	var freq = char_fx.env.get("freq", 2.0)
+	var count = char_fx.env.get("count", 5)
+	var sinedTime = (sin(char_fx.elapsed_time * freq) + 1.0) / 2.0
+	var y_off = sinedTime * height
+	color.a = 1.0
+	char_fx.color = char_fx.color.linear_interpolate(color, sinedTime)
+	char_fx.offset = Vector2(0, -1) * y_off
+	return true
