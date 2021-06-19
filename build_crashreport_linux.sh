d88b25e114015bd2e9d88c35a68a8030b2d7ffd4
JezSonic <karoltomaszewskimusic@gmail.com>
2021-06-20 01:59:13 +0200

    Update build_crashreport_linux.sh

--- build_crashreport_linux.sh
+++ build_crashreport_linux.sh
@@ -1,1 +1,1 @@
-pyinstaller --onedir send_log.py --collect-data "easygui" --collect-data "requests" --distpath "./crashreport/x11/dist" --workpath "./crashreport/x11/build"
+pyinstaller --onedir send_log.py --collect-data "requests" --distpath "./crashreport/x11/dist" --workpath "./crashreport/x11/build"
