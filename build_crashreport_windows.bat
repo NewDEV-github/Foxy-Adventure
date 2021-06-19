a8471d33fbbe65e583aedc44e7be926349304909
JezSonic <karoltomaszewskimusic@gmail.com>
2021-06-20 01:50:18 +0200

    Update build_crashreport_windows.bat

--- build_crashreport_windows.bat
+++ build_crashreport_windows.bat
@@ -1,1 +1,1 @@
-pyinstaller --onedir send_log.py --collect-data "easygui" --collect-data "requests" --distpath "crashreport/win/dist" --workpath "crashreport/win/build"
+pyinstaller --onedir send_log.py --collect-data "requests" --distpath "crashreport/win/dist" --workpath "crashreport/win/build"
