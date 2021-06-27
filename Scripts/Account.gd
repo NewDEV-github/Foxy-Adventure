4901b4dcb0c5d3aff5000db9ca04a20f39a54218
JezSonic <karoltomaszewskimusic@gmail.com>
2021-06-27 14:17:30 +0200

    add sdk build script

--- Scripts/Account.gd
+++ Scripts/Account.gd
@@ -6,14 +6,7 @@ extends Panel
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called when the node enters the scene tree for the first
 
 func load_account_info():
 #	print(Globals.user_data['displayname'])
