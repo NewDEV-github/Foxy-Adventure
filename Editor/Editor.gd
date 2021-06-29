e0b143eed7751ce0c28da24d25a80abeba0a2cc1
JezSonic <karoltomaszewskimusic@gmail.com>
2021-06-29 16:04:01 +0200

    add some basic code for loading audio from files ad setting backgrounds to stages

--- Editor/Editor.gd
+++ Editor/Editor.gd
@@ -5,7 +5,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-
+var modes = ["custom_audio", "custom_tiles", "finished"]
 signal stage_preloaded
 var audio_file_path:String = ""
 var scripts:Array = []
@@ -20,11 +20,26 @@ func load_stage(path):
 	var p_scn = PackedScene.new()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TileMap.paint_tile(Vector2(100, 100), 4)
+	var dir = Directory.new()
+	var mode_num = 0
+	var mode = modes[mode_num]
+	if not dir.dir_exists("user://level_data/"):
+		dir.make_dir("user://level_data/")
+	if not dir.dir_exists("user://level_data/" + Globals.level_name):
+		dir.open("user://level_data/")
+		dir.make_dir(Globals.level_name)
+	while not mode == "finished":
+		dir.open("user://level_data/" + Globals.level_name)
+		if not dir.dir_exists(mode):
+			dir.make_dir(mode)
+		mode_num += 1
+		mode = modes[mode_num]
 func clear_all():
 	if $TileMap:
 		$TileMap.clear()
-
+func add_bg(file_path:String):
+	var n = load(file_path).instance()
+	add_child(n)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("paint_tile"):
@@ -35,7 +50,6 @@ func build_level():
 	var dir = Directory.new()
 	#custom_audio
 	var mode_num = 0
-	var modes = ["custom_audio", "custom_tiles", "finished"]
 	var mode = modes[mode_num]
 	while not mode == "finished":
 		dir.open("user://level_data/" + Globals.level_name + "/%s/" % mode)
@@ -47,12 +61,19 @@ func build_level():
 				pck.add_file(Globals.level_name + "/%s/" + file_name.get_file() % mode, "user://level_data/" + Globals.level_name + "/%s/" + file_name.get_file() % mode)
 			file_name = dir.get_next()
 		mode_num += 1
-		mode[mode_num]
+		mode = modes[mode_num]
 	pck.flush()
+	OS.alert("Level built to path:\n" + Globals.level_path + "/" + Globals.level_name + ".pck")
 func assign_script_to_scene(script_path):
 	$".".set_script(script_path)
 func save_level():
 	var p_scn = PackedScene.new()
-	p_scn.pack($".")
-	p_scn.pack($TileMap)
+	p_scn.pack(get_node("."))
+	p_scn.pack(get_node("TileMap"))
 	ResourceSaver.save(Globals.level_path + "/" + Globals.level_name + ".tscn", p_scn)
+func add_audio_from_file(path:String):
+	var dir = Directory.new()
+	dir.copy(path, "user://level_data/" + Globals.level_name + "/custom_audio/" + path.get_file())
+	var audio = load("user://level_data/" + Globals.level_name + "/custom_audio/" + path.get_file())
+#	var stream = AudioStream.new()
+	$AudioStreamPlayer.set_stream(audio)
--- Editor/ui/navbar.gd
+++ Editor/ui/navbar.gd
@@ -10,12 +10,14 @@ onready var audio_menu = $Container/Audio.get_popup()
 onready var bg_menu = $Container/Bg.get_popup()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	refresh_bg_list()
 	bg_menu.connect("id_pressed", self, "bg_menu_selected")
 	file_menu.connect("id_pressed", self, "file_menu_selected")
 	audio_menu.connect("id_pressed", self, "audio_menu_selected")
-
+var bg_file_paths = {"Laboratory": "res://Scenes/Stages/bg1.tscn"}
 func bg_menu_selected(id:int):
-	pass
+	if id == 0:
+		$BackgroundPanel.popup_centered()
 
 func file_menu_selected(id:int):
 	if id == 0:
@@ -24,6 +26,24 @@ func file_menu_selected(id:int):
 		$FileLoadPopup.popup_centered()
 	if id == 2:
 		Editor.save_level()
-
+	if id == 4:
+		Editor.build_level()
+func add_bg_to_list(name:String, file_path:String):
+	$BackgroundPanel/VBoxContainer/BgList.add_item(name)
+	bg_file_paths[name] = file_path
+	refresh_bg_list()
 func audio_menu_selected(id:int):
-	pass
+	if id == 0:
+		$AudioPopup.popup_centered()
+func refresh_bg_list():
+	$BackgroundPanel/VBoxContainer/BgList.clear()
+	for i in bg_file_paths:
+		$BackgroundPanel/VBoxContainer/BgList.add_item(i)
+
+
+func _on_BgList_item_activated(index):
+	Editor.add_bg(bg_file_paths[$BackgroundPanel/VBoxContainer/BgList.get_item_text(index)])
+
+
+func _on_AudioPopup_file_selected(path):
+	Editor.add_audio_from_file(path)
