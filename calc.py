925323fff52765e052fa186ceeb17bd67c1539f8
JezSonic <karoltomaszewskimusic@gmail.com>
2021-07-08 22:27:43 +0200

    git doesn't work

---
+++ calc.py
@@ -0,0 +1,2 @@
+a = input("Liczba 'a': ") #prosi uzytkownika o wpisanie liczby a i przypisuje ja do zmiennej a
+b = input("Liczba 'b': ") #prosi uzytkownika o wpisanie liczby b i przypisuje ja do zmiennej b
