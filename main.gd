593dc09a556269f69f4a5ca741964c7280f4f9d7
JezSonic <karoltomaszewskimusic@gmail.com>
2021-07-10 19:26:05 +0200

    fixes for sdk

--- main.gd
+++ main.gd
@@ -5,10 +5,12 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var sdk = FoxyAdventureSDK.new()
-
+var sdk
+func init_mod():
+	sdk = FoxyAdventureSDK.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	sdk = FoxyAdventureSDK.new()
 	sdk.init(sdk.INIT_FLAGS.INIT_DEBUG)
 	print(str(sdk.get_lifes()))
 func add_stages():
