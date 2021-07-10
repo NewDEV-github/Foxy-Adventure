7b135ae4a90c930f7db068e9a2a226223ebab555
JezSonic <karoltomaszewskimusic@gmail.com>
2021-07-10 20:41:55 +0200

    fixes for sdk

--- main.gd
+++ main.gd
@@ -8,15 +8,9 @@ extends Control
 var sdk
 func init_mod():
 	sdk = FoxyAdventureSDK.new()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	sdk = FoxyAdventureSDK.new()
 	sdk.init(sdk.INIT_FLAGS.INIT_DEBUG)
 	print(str(sdk.get_lifes()))
-func add_stages():
 	sdk.register_world("Test world", "res://test_scenes/worlds/test_world.tscn")
-
-func add_characters():
 	sdk.register_character("Test character", "res://test_scenes/characters/test_character.tscn")
 
 """For more examples, please, take a look at: https://github.com/NewDEV-github/Foxy-Adventure/wiki/SDK"""
