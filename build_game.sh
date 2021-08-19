ba5228b969b04ff92a89c8a22f229ce900917677
JezSonic <karoltomaszewskimusic@gmail.com>
2021-08-19 09:50:04 +0200

    update build_game.sh

--- build_game.sh
+++ build_game.sh
@@ -50,7 +50,7 @@ sudo mkdir -p .local/share/godot
 cd .local/share/godot
 sudo mkdir -p templates/$GODOT_VERSION
 sudo cp -r $GITHUB_WORKSPACE/templates/* templates/$GODOT_VERSION
-if [$MODE -eq "basegame"] || [$MODE -eq "all"]
+if [$MODE -eq "all"]
 then
   cd $GITHUB_WORKSPACE
   sudo mkdir -p "builds/"$OS"-"$BITS"-standard"
