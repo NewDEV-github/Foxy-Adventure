c9c53de7b5fc90caa46bba1064e9665dd52d9a92
JezSonic <karoltomaszewskimusic@gmail.com>
2021-08-19 20:04:25 +0200

    update build_game.sh

--- build_game.sh
+++ build_game.sh
@@ -1,40 +1,8 @@
 #!/bin/bash
 sudo apt-get install unzip
-OS="linux"
-BITS="64"
-EXEC_EXT="x86_64"
-GODOT_VERSION="3.3.2.stable"
-MODE="all"
-while getopts "platform:bits:godot-version:mode" opt
-do
-   case "$opt" in
-      platform ) OS="$OPTARG" ;;
-      bits ) BITS="$OPTARG" ;;
-      godot-version ) GODOT_VERSION="$OPTARG" ;;
-      mode ) MODE="$OPTARG" ;;
-   esac
-done
-echo "Building for platform: "$OS", "$BITS"-bits, with Godot Engine version: "$GODOT_VERSION". Build mode: "$MODE
-if ["$OS" = "linux"]; then
-  if ["$BITS" = "64"]; then
-    EXEC_EXT="x86_64"
-  fi
-  if ["$BITS" = "32"]; then
-    EXEC_EXT="x86"
-  fi
-fi
-if ["$OS" = "windows"]; then
-  EXEC_EXT="exe"
-fi
-if ["$OS" = "osx"]; then
-  EXEC_EXT=".zip"
-fi
-
-version=$(echo $GODOT_VERSION | grep -o '[^-]*$')
-GODOT_VERSION_NUMBER=$(echo $version | cut -d. -f1)"."$(echo $version | cut -d. -f2)"."$(echo $version | cut -d. -f3) #3.3.3
-GODOT_VERSION_VERSION=$(echo $version | cut -d. -f4) #stable
-GODOT_BINARY_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/"$GODOT_VERSION_NUMBER"/Godot_v"$GODOT_VERSION_NUMBER"-"$GODOT_VERSION_VERSION"_linux_headless.64.zip"
-GODOT_EXPORT_TEMPLATES_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/"$GODOT_VERSION_NUMBER"/Godot_v"$GODOT_VERSION_NUMBER"-"$GODOT_VERSION_VERSION"_export_templates.tpz"
+GODOT_VERSION="3.3.3.stable"
+GODOT_BINARY_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/3.3.2/Godot_v3.3.3-stable_linux_headless.64.zip"
+GODOT_EXPORT_TEMPLATES_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/3.3.3/Godot_v3.3.3-stable_export_templates.tpz"
 
 GODOT_BINARY_FILENAME="$(basename -s .zip $GODOT_BINARY_DOWNLOAD_LINK)"
 GODOT_ZIP_FILENAME="$(basename $GODOT_BINARY_DOWNLOAD_LINK)"
@@ -50,8 +18,10 @@ sudo mkdir -p .local/share/godot
 cd .local/share/godot
 sudo mkdir -p templates/$GODOT_VERSION
 sudo cp -r $GITHUB_WORKSPACE/templates/* templates/$GODOT_VERSION
-if ["$MODE" = "all"]; then
-  cd $GITHUB_WORKSPACE
-  sudo mkdir -p "builds/"$OS"-"$BITS"-standard"
-  sudo ./$GODOT_BINARY_FILENAME --path "." --export $OS"-"$BITS $GITHUB_WORKSPACE"/builds/"$OS"-"$BITS"-standard/FoxyAdventure."$EXEC_EXT
-fi
+
+cd $GITHUB_WORKSPACE
+sudo mkdir -p builds/{x11-64-standard,win-64-standard,osx-standard}
+
+sudo ./$GODOT_BINARY_FILENAME --path "." --export "x11-64" $GITHUB_WORKSPACE/builds/x11-64-standard/FoxyAdventure.x86_64
+sudo ./$GODOT_BINARY_FILENAME --path "." --export "osx" $GITHUB_WORKSPACE/builds/osx-standard/FoxyAdventure.zip
+sudo ./$GODOT_BINARY_FILENAME --path "." --export "win-64" $GITHUB_WORKSPACE
