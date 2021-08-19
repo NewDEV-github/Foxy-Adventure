69791635f06f4147b88265456564642cfd045282
JezSonic <karoltomaszewskimusic@gmail.com>
2021-08-19 09:19:33 +0200

    Fix and add echo for checking script arguments

--- build_game.sh
+++ build_game.sh
@@ -14,7 +14,7 @@ do
       mode ) MODE="$OPTARG" ;;
    esac
 done
-echo "Building for platform: " + $OS + ", " + $BITS + "-bits, with Godot Engine version: " + $GODOT_VERSION + ". Build mode: " $MODE
+echo "Building for platform: "$OS", "$BITS"-bits, with Godot Engine version: "$GODOT_VERSION". Build mode: " $MODE
 if $OS == "linux"
 then
   if $BITS == "64"
@@ -36,10 +36,10 @@ then
 fi
 
 version=$(echo $GODOT_VERSION | grep -o '[^-]*$')
-GODOT_VERSION_NUMBER=$(echo $version | cut -d. -f1)+"."+$(echo $version | cut -d. -f2)+"."+$(echo $version | cut -d. -f3) #3.3.3
+GODOT_VERSION_NUMBER=$(echo $version | cut -d. -f1)"."$(echo $version | cut -d. -f2)"."$(echo $version | cut -d. -f3) #3.3.3
 GODOT_VERSION_VERSION=$(echo $version | cut -d. -f4) #stable
-GODOT_BINARY_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/" + $GODOT_VERSION_NUMBER+"/Godot_v" + $GODOT_VERSION_NUMBER+ "-" + $GODOT_VERSION_VERSION+"_linux_headless.64.zip"
-GODOT_EXPORT_TEMPLATES_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/" + $GODOT_VERSION_NUMBER+"/Godot_v" + $GODOT_VERSION_NUMBER+ "-" + $GODOT_VERSION_VERSION+"_export_templates.tpz"
+GODOT_BINARY_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/"$GODOT_VERSION_NUMBER"/Godot_v"$GODOT_VERSION_NUMBER"-"$GODOT_VERSION_VERSION"_linux_headless.64.zip"
+GODOT_EXPORT_TEMPLATES_DOWNLOAD_LINK="https://downloads.tuxfamily.org/godotengine/"$GODOT_VERSION_NUMBER+"/Godot_v"$GODOT_VERSION_NUMBER+ "-"$GODOT_VERSION_VERSION+"_export_templates.tpz"
 
 GODOT_BINARY_FILENAME="$(basename -s .zip $GODOT_BINARY_DOWNLOAD_LINK)"
 GODOT_ZIP_FILENAME="$(basename $GODOT_BINARY_DOWNLOAD_LINK)"
