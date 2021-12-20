e9cb272ea3c0ceab1f1917e3d349f3b46cd71f36
JezSonic <karotomaszewskimusic@gmail.com>
2021-12-20 12:49:07 +0100

    upodater???

---
+++ GithubUpdate.gd
@@ -0,0 +1,24 @@
+extends Node
+var request_url = "https://api.github.com/repos/NewDEV-github/Foxy-Adventure/commits"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var latest_release_url = ""
+var base_patch_url = "https://github.com/NewDEV-github/Foxy-Adventure/releases/download/%s/%s"
+var platforms = {
+	"Windows": "build-win-64-3.4.zip",
+	"OSX": "build-osx-3.4.zip",
+	"X11": "build-x11-64-3.4.zip"
+}
+func _ready():
+	$HTTPRequest.request(request_url, [], true, HTTPClient.METHOD_GET)
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	var commits = json.result
+	var latest_tag = commits[0]['sha'].left(6)
+	print(latest_tag)
+	print("Latest release URL: " + (base_patch_url % [latest_tag, platforms[OS.get_name()]]))
+		
