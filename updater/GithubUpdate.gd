999e8d1ad4cc750265385d34802b6ef7c3d5eb69
JezSonic <karotomaszewskimusic@gmail.com>
2021-12-24 10:19:45 +0100

    little improvements

--- updater/GithubUpdate.gd
+++ updater/GithubUpdate.gd
@@ -1,28 +1,27 @@
-extends Control
+extends WindowDialog
 var latest_release_data_url= "https://api.github.com/repos/NewDEV-github/Foxy-Adventure/releases/latest"
+var latest_tag = ""
 var platforms = {
 	"Windows": "build-win-64-3.4.zip",
 	"OSX": "build-osx-3.4.zip",
 	"X11": "build-x11-64-3.4.zip"
 }
-var tag_limit = 4
-var _tmp_tag_download_info_result_notes = ""
-var _tmp_tag_download_info:String = ""
-var _tmp_tag_download_info_result={}
+
 func _ready():
 	$HTTPRequest.request(latest_release_data_url, ["Accept: application/vnd.github.v3+json"], true, HTTPClient.METHOD_GET)
-var updates_data = {}
-var releases_data = {}
-var release_notes_data = {}
-var commits_data = []
+
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
 	var data = json.result
-	$Main.text = "Update " + data['name'] + " is avaliable"
+	$Main.text = "Checking for updates"
 	for i in data.keys():
 		print(str(i) + " = " + str(data[i]))
 	$TagData.bbcode_text = data['body']
-	compare_versions(data['tag_name'])
+	latest_tag = data['tag_name']
+	if compare_versions(data['tag_name']):
+		$Main.text = "You have the newest version"
+	else:
+		$Main.text = "Update " + data['name'] + " is avaliable"
 
 func compare_versions(version:String):
 	var f= File.new()
@@ -37,3 +36,11 @@ func compare_versions(version:String):
 		print('not up to date')
 		return false
 
+func _on_Download_pressed():
+	var url = "https://github.com/NewDEV-github/Foxy-Adventure/releases/download/%s/%s" % [latest_tag, platforms[OS.get_name()]]
+	Directory.new().make_dir_recursive("user://updates/%s" % [latest_tag])
+	Directory.new().make_dir_recursive("user://updates/_tmp/%s" % [latest_tag])
+	if not File.new().file_exists("user://updates/%s/%s" % [latest_tag, platforms[OS.get_name()]]):
+		$patch_downloader.download_file = "user://updates/%s/%s" % [latest_tag, platforms[OS.get_name()]]
+		$patch_downloader.request(url)
+	Unzip.unzip("user://updates/%s/%s" % [latest_tag, platforms[OS.get_name()]],"user://updates/_tmp/%s" % [latest_tag])
