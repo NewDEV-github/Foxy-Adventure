adbea2bc02522c6f8bb9d5b1f7e87506af85e418
JezSonic <karotomaszewskimusic@gmail.com>
2022-01-06 17:57:09 +0100

    add some python libs and python bugtracker

---
+++ addons/github_issues_bugtracker/issues_api.py
@@ -0,0 +1,33 @@
+from godot import exposed, export
+from godot import *
+import json
+import requests
+
+@exposed(tool=True)
+class issues_api(Control):
+
+	# member variables here, example:
+	a = export(int)
+	b = export(str, default='foo')
+	
+
+
+	
+	def make_github_issue(self, title, body=None, labels=None):
+		'''Create an issue on github.com using the given parameters.'''
+		# Our url to create issues via POST
+		url = 'https://api.github.com/repos/%s/%s/issues' % ('NewDEV-github', "Foxy-Adventure")
+		# Create an authenticated session to create the issue
+		session = requests.session()
+		session.auth = requests.auth.HTTPDigestAuth('JezSonic', 'DoS2021123')
+		# Create our issue
+		issue = {'title': title,
+				'body': body,
+				'labels': labels}
+		# Add the issue to our repository
+		r = session.post(url, json.dumps(issue), headers={'Authorization':'token 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'})
+		if r.status_code == 201:
+			print('Successfully created Issue "%s"' % title)
+		else:
+			print('Could not create Issue "%s"' % title)
+			print('Response:', r.content)
